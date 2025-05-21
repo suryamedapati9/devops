dc35f393719e52bf7d84fa2589a24c55034e34f8
unknown <ssr.medapati98@gmail.com>
2025-05-21 03:38:07 -0500

    automation script to launch aws instances

--- roboshop-scripts/roboshop.sh
+++ roboshop-scripts/roboshop.sh
@@ -4,13 +4,12 @@ AMI_ID="ami-09c813fb71547fc4f"
 SG_ID="sg-03c5041a4b9576735" # replace with your SG ID
 INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "frontend")
 ZONE_ID="Z03684891XLNU1WXXH756" # replace with your ZONE ID Z03684891XLNU1WXXH756
-
 DOMAIN_NAME="mydevopspractice.store" # replace with your domain
 
 #for instance in ${INSTANCES[@]}
 for instance in ${INSTANCES[@]}
 do
-    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-01bc7ebe005fb1cb2 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-03c5041a4b9576735 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
     if [ $instance != "frontend" ]
     then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
